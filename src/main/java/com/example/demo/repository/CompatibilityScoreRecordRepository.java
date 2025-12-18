e9eb3f4a573dd25712bc2e793a095ea8d7d083c7
bala-amypo <bala@amypo.in>
2025-12-18 16:13:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
+++ src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CompatibilityScoreRecordRepository {
+public interface CompatibilityScoreRecordRepository extends JpaRepository<Location,Long> {
 
 }
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.entity.Location;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
