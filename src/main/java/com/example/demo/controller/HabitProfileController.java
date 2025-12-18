daf786bfa22c9871921881a2f0a3062f470cb67e
bala-amypo <bala@amypo.in>
2025-12-18 17:16:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/HabitProfileController.java
+++ src/main/java/com/example/demo/controller/HabitProfileController.java
@@ -1,5 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.HabitProfile;
+import com.example.demo.service.HabitProfileService;
+
+@RestController
+@RequestMapping("/habits")
 public class HabitProfileController {
 
+    private final HabitProfileService service;
+
+    public HabitProfileController(HabitProfileService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public HabitProfile saveHabit(@RequestBody HabitProfile habitProfile) {
+        return service.saveHabitProfile(habitProfile);
+    }
 }
--- src/main/java/com/example/demo/service/impl/HabitprofileServiceImplement.java
+++ src/main/java/com/example/demo/service/impl/HabitprofileServiceImplement.java
@@ -1,5 +1,29 @@
-package com.example.demo.service.implement;
+package com.example.demo.service.impl;
 
-public class HabitprofileServiceImplement {
-    
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.HabitProfile;
+import com.example.demo.exception.InvalidStudyHoursException;
+import com.example.demo.repository.HabitProfileRepository;
+import com.example.demo.service.HabitProfileService;
+
+@Service
+public class HabitProfileServiceImpl implements HabitProfileService {
+
+    private final HabitProfileRepository repository;
+
+    public HabitProfileServiceImpl(HabitProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public HabitProfile saveHabitProfile(HabitProfile habitProfile) {
+
+        if (habitProfile.getStudyHoursPerDay() == null
+                || habitProfile.getStudyHoursPerDay() < 0) {
+            throw new InvalidStudyHoursException("study hours invalid");
+        }
+
+        return repository.save(habitProfile);
+    }
 }
