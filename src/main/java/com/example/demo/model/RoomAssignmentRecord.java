f96a3734715945a58439c1726b29f8e68164cc83
bala-amypo <bala@amypo.in>
2025-12-18 02:08:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RoomAssignmentRecord.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.RoomAssignmentRecord;
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+public class RoomAssignmentRecord{
+    private Long id;
+    private String roomNumber;
+    private Long studentAId;
+     private Long studentBId;
+    private LocalDateTime assignedAt;
+
+    @Enumerated(EnumType.STRING)
+    private RoomAssignment
+}
