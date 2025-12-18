992fba09d119b29c9d57b0a5f904685714246027
bala-amypo <bala@amypo.in>
2025-12-18 05:00:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/enum/CompatibilityLevel.java
@@ -0,0 +1,5 @@
+package com.example.demo.model.enums;
+
+public enum CompatibilityLevel {
+    LOW, MEDIUM, HIGH, EXCELLENT
+}
---
+++ src/main/java/com/example/demo/model/enum/StudyHours.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.enums;
+
+public enum StudyHours {
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT
+}
