9a0cff984550127173f3ede62ff5cfa46fca9d59
bala-amypo <bala@amypo.in>
2025-12-19 03:04:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/MatchAttemptRecordRepository.java
+++ src/main/java/com/example/demo/repository/MatchAttemptRecordRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.MatchAttemptRecord;
+import com.example.demo.model.MatchAttemptRecord;
 
 public interface MatchAttemptRecordRepository extends JpaRepository<MatchAttemptRecord,Long> {
 
--- src/main/java/com/example/demo/repository/RoomAssignmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/RoomAssignmentRecordRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity. RoomAssignmentRecord;
+import com.example.demo.model. RoomAssignmentRecord;
 
 public interface RoomAssignmentRecordRepository extends JpaRepository< RoomAssignmentRecord,Long> {
 
