17c85bf9230d7a8ad820d2bf931a72bdfa5c2ba5
bala-amypo <bala@amypo.in>
2025-12-24 16:53:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/HabitProfileRepository.java
+++ src/main/java/com/example/demo/repository/HabitProfileRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.model.HabitProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-@Repository
 public interface HabitProfileRepository
         extends JpaRepository<HabitProfile, Long> {
 
