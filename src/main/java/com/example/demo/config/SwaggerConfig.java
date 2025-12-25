beef9b2138964352e08342f489e9f47dec5ce709
bala-amypo <bala@amypo.in>
2025-12-25 08:40:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,29 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
-
-
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI api() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Hostel Roommate Compatibility Matcher API")
-                        .version("1.0"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9043.32procr.amypo.ai/")
+                ));
+        }
 }
-
-
-
-
-
-
